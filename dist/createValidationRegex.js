978be9ba2515c49bdbc5532f834ba896ea456832
woojoung <ywj981217@naver.com>
2025-03-16 20:57:22 +0900

    build: update package configuration and dependencies

---
+++ dist/createValidationRegex.js
@@ -0,0 +1,43 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.createPhoneNumberRegex = exports.createPasswordRegex = exports.createEmailRegex = exports.createDateRegex = void 0;
+// src/createValidationRegex.js
+
+var createPasswordRegex = exports.createPasswordRegex = function createPasswordRegex(_ref) {
+  var minLength = _ref.minLength,
+    uppercase = _ref.uppercase,
+    lowercase = _ref.lowercase,
+    digits = _ref.digits,
+    specialChar = _ref.specialChar;
+  var pattern = '^';
+  if (minLength) {
+    pattern += "(?=.{".concat(minLength, ",})"); // 최소 길이
+  }
+  if (uppercase) {
+    pattern += '(?=.*[A-Z])'; // 대문자 포함
+  }
+  if (lowercase) {
+    pattern += '(?=.*[a-z])'; // 소문자 포함
+  }
+  if (digits) {
+    pattern += '(?=.*\\d)'; // 숫자 포함
+  }
+  if (specialChar) {
+    pattern += '(?=.*[!@#$%^&*(),.?":{}|<>])'; // 특수문자 포함
+  }
+  pattern += '[A-Za-z\\d!@#$%^&*(),.?":{}|<>]+$'; // 가능한 문자 (대소문자, 숫자, 특수문자)
+
+  return pattern;
+};
+var createEmailRegex = exports.createEmailRegex = function createEmailRegex() {
+  return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+};
+var createPhoneNumberRegex = exports.createPhoneNumberRegex = function createPhoneNumberRegex() {
+  return /^\+?[1-9]\d{1,14}$/;
+};
+var createDateRegex = exports.createDateRegex = function createDateRegex() {
+  return /^(?:\d{4})-(?:0[1-9]|1[0-2])-(?:0[1-9]|[12][0-9]|3[01])$/;
+};
---
+++ dist/index.js
@@ -0,0 +1,30 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "createDateRegex", {
+  enumerable: true,
+  get: function get() {
+    return _createValidationRegex.createDateRegex;
+  }
+});
+Object.defineProperty(exports, "createEmailRegex", {
+  enumerable: true,
+  get: function get() {
+    return _createValidationRegex.createEmailRegex;
+  }
+});
+Object.defineProperty(exports, "createPasswordRegex", {
+  enumerable: true,
+  get: function get() {
+    return _createValidationRegex.createPasswordRegex;
+  }
+});
+Object.defineProperty(exports, "createPhoneNumberRegex", {
+  enumerable: true,
+  get: function get() {
+    return _createValidationRegex.createPhoneNumberRegex;
+  }
+});
+var _createValidationRegex = require("./createValidationRegex");
